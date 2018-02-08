0921ef981f2cbfe2770567c372205524b2ceba5a
Timo Stollenwerk <tisto@plone.org>
2018-02-08 14:35:13 +0100

    Add minimal react router implementation.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,30 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { NavLink, Switch, Route } from 'react-router-dom';
+
+const Home = () => (
+  <div className="home">
+    <h1>Welcome</h1>
+    <p>Welcome to my page!</p>
+  </div>
+);
+
+const About = () => (
+  <div className="about">
+    <h1>About Me</h1>
+    <p>This is about me!</p>
+  </div>
+);
+
+const Contact = () => (
+  <div className="contact">
+    <h1>Contact Me</h1>
+    <p>Contact me via email!</p>
+  </div>
+);
 
 class App extends Component {
-
   constructor() {
     super();
     this.state = {
@@ -12,21 +33,19 @@ class App extends Component {
   }
 
   componentDidMount() {
-    fetch(
-      'http://127.0.0.1:8000/users/1',
-      {
-        headers: {
-          'Accept': 'application/json'
-        }
+    fetch('http://127.0.0.1:8000/users/1', {
+      headers: {
+        Accept: 'application/json'
       }
-    ).then((response) => response.json())
-    .then((responseData) => {
-      this.setState({ user: responseData });
-      console.log('Fetch from backend successful!')
     })
-    .catch((error) => {
-      console.log('Error fetching and parsing data', error);
-    });
+      .then(response => response.json())
+      .then(responseData => {
+        this.setState({ user: responseData });
+        console.log('Fetch from backend successful!');
+      })
+      .catch(error => {
+        console.log('Error fetching and parsing data', error);
+      });
   }
 
   render() {
@@ -36,11 +55,24 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>Username: {this.state.user.username}</p>
-        <p>E-Mail: {this.state.user.email}</p>
+        <nav>
+          <ul>
+            <li>
+              <NavLink to="/">Home</NavLink>
+            </li>
+            <li>
+              <NavLink to="/about">About</NavLink>
+            </li>
+            <li>
+              <NavLink to="/contact">Contact</NavLink>
+            </li>
+          </ul>
+        </nav>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/about" component={About} />
+          <Route exact path="/contact" component={Contact} />
+        </Switch>
       </div>
     );
   }
