bfbefc91fb9436e895d027cbe90cf18785b13f71
Timo Stollenwerk <tisto@plone.org>
2018-02-08 16:08:24 +0100

    Fix router test by using MemoryRouter.

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,16 +1,20 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import App from './App';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import { MemoryRouter } from 'react-router';
 
 it('works', () => {
   const myvar = 1;
   expect(myvar).toEqual(1);
 });
 
-/*
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(
+    <MemoryRouter>
+      <App />
+    </MemoryRouter>,
+    div
+  );
   ReactDOM.unmountComponentAtNode(div);
 });
-*/
