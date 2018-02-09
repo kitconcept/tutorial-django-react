6222c573c7bc4a06ab506806ffc7f9c6b09ef1aa
Timo Stollenwerk <tisto@plone.org>
2018-02-09 08:40:00 +0100

    Add first api implemention (WIP).

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,15 +1,19 @@
-import { createStore, applyMiddleware, compose } from 'redux';
-import { routerMiddleware } from 'react-router-redux';
-import thunk from 'redux-thunk';
 import createHistory from 'history/createBrowserHistory';
 import { combineReducers } from 'redux';
 import { routerReducer } from 'react-router-redux';
+import createLogger from 'redux-logger';
+import { createStore, compose, applyMiddleware } from 'redux';
+import { routerMiddleware } from 'react-router-redux';
+import thunk from 'redux-thunk';
+
+import { api } from './middleware';
+import reducer from './reducers';
 
 export const history = createHistory();
 
 const initialState = {};
 const enhancers = [];
-const middleware = [thunk, routerMiddleware(history)];
+const middleware = [routerMiddleware(history), thunk, api];
 
 if (process.env.NODE_ENV === 'development') {
   const devToolsExtension = window.devToolsExtension;
@@ -19,12 +23,39 @@ if (process.env.NODE_ENV === 'development') {
   }
 }
 
-const composedEnhancers = compose(applyMiddleware(...middleware), ...enhancers);
+/**
+ * AddToMiddleWare.
+ * @function addToMiddleWare
+ * @param {Array} data Middleware Array
+ * @param {Object} logger Logger instance
+ * @returns {Array} middleware.
+ */
+const addToMiddleWare = (data, logger) => {
+  return data;
+};
 
-const rootReducer = combineReducers({
-  routing: routerReducer
-});
+/**
+ * Configure store.
+ * @function configureStore
+ * @param {Object} initialState state object
+ * @param {Object} history state object
+ * @param {bool} logger showLogger
+ * @param {Object} apiHelper ApiHelper
+ * @returns {Object} Store.
+ */
+export default function configureStore(
+  initialState,
+  history,
+  logger,
+  apiHelper
+) {
+  const middlewares = addToMiddleWare(
+    [applyMiddleware(routerMiddleware(history), thunk, api(apiHelper))],
+    logger
+  );
 
-const store = createStore(rootReducer, initialState, composedEnhancers);
+  const createStoreWithMiddleware = compose(...middlewares)(createStore);
+  const store = createStoreWithMiddleware(reducer, initialState);
 
-export default store;
+  return store;
+}
