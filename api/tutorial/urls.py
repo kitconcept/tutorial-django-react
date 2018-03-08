41129e7a5aa6bc828d7ef2f8b1e76dd99477a78d
Timo Stollenwerk <tisto@plone.org>
2018-03-08 11:44:36 +0100

    Add basic jwt to Django.

--- api/tutorial/urls.py
+++ api/tutorial/urls.py
@@ -1,5 +1,8 @@
 from django.conf.urls import url, include
 from rest_framework import routers
+from rest_framework_jwt.views import obtain_jwt_token
+from rest_framework_jwt.views import refresh_jwt_token
+from rest_framework_jwt.views import verify_jwt_token
 from tutorial.quickstart import views
 
 router = routers.DefaultRouter()
@@ -10,5 +13,8 @@ router.register(r'groups', views.GroupViewSet)
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
     url(r'^', include(router.urls)),
-    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    url(r'^api-token-auth/', obtain_jwt_token),
+    url(r'^api-token-refresh/', refresh_jwt_token),
+    url(r'^api-token-verify/', verify_jwt_token),
 ]
